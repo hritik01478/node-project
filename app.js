4b9424e8ded333935f9eba03a2b8aa9ca23ddef8
hritik01478 <hritikkumar01478@gmail.com>
2022-06-11 23:09:21 +0530

    made add, delete and view a blog

--- app.js
+++ app.js
@@ -1,7 +1,9 @@
+const { render } = require('ejs');
 const express = require('express');
 const { default: mongoose } = require('mongoose');
 const morgan = require('morgan');
 const Blog = require('./models/blog');
+const blogRoutes = require('./routes/blogRoutes');
 
 //connect to  mongodb
 const dbURI = 'mongodb+srv://hritik:01478@nodetut.eaeh7.mongodb.net/note-tuts?retryWrites=true&w=majority';
@@ -16,72 +18,32 @@ app.set('view engine', 'ejs');
 
 //middleware and static files
 app.use((morgan('dev')));
+app.use(express.urlencoded({ extended: true }));
 app.use(express.static('public'));
 
 //mongoose and mongo sandbox routes
-app.get('/add-blog', (req, res) => {
-    const blog = new Blog({
-        title: 'new blog 2',
-        snippet: 'about my new blog',
-        body: 'more about my new blog'
-    })
 
-    blog.save()
-        .then((result) => {
-            res.send(result)
-        })
-        .catch((err) => {
-            console.log(err)
-        })
-})
-app.get('/all-blogs', (req, res) => {
-    Blog.find()
-        .then((result) => {
-            res.send(result);
-        })
-        .catch(err => {
-            console.log(err);
-        })
-})
-app.get('/single-blog', (req, res) => {
-    Blog.findById('62a45fa319bc2172eb481397')
-        .then((result) => {
-            res.send(result);
-        })
-        .catch(err => {
-            console.log(err);
-        })
-})
 //routes
 app.get('/', (req, res) => {
     res.redirect('/blogs');
 });
 
-app.get('/', (req, res) => {
-    const blogs = [
-        { title: 'Hritik: wahi ladka jo urmila se ghumta tha', snippet: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Aliquid ex ab hic! Aliquam ab tempora in adipisci eos cumque, error odit laudantium sed obcaecati. Quam alias, vel perferendis nisi, dignissimos quod illum odit esse dolorem et impedit omnis. Eos, aliquam?' },
-        { title: 'Lazy Avnit', snippet: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Aliquid ex ab hic! Aliquam ab tempora in adipisci eos cumque, error odit laudantium sed obcaecati. Quam alias, vel perferendis nisi, dignissimos quod illum odit esse dolorem et impedit omnis. Eos, aliquam?' },
-        { title: 'Brijesh the Ashiq', snippet: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Aliquid ex ab hic! Aliquam ab tempora in adipisci eos cumque, error odit laudantium sed obcaecati. Quam alias, vel perferendis nisi, dignissimos quod illum odit esse dolorem et impedit omnis. Eos, aliquam?' }
-    ]
-    res.render('index', { title: "Home", blogs });
-});
+// app.get('/', (req, res) => {
+//     const blogs = [
+//         { title: 'Hritik: wahi ladka jo urmila se ghumta tha', snippet: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Aliquid ex ab hic! Aliquam ab tempora in adipisci eos cumque, error odit laudantium sed obcaecati. Quam alias, vel perferendis nisi, dignissimos quod illum odit esse dolorem et impedit omnis. Eos, aliquam?' },
+//         { title: 'Lazy Avnit', snippet: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Aliquid ex ab hic! Aliquam ab tempora in adipisci eos cumque, error odit laudantium sed obcaecati. Quam alias, vel perferendis nisi, dignissimos quod illum odit esse dolorem et impedit omnis. Eos, aliquam?' },
+//         { title: 'Brijesh the Ashiq', snippet: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Aliquid ex ab hic! Aliquam ab tempora in adipisci eos cumque, error odit laudantium sed obcaecati. Quam alias, vel perferendis nisi, dignissimos quod illum odit esse dolorem et impedit omnis. Eos, aliquam?' }
+//     ]
+//     res.render('index', { title: "Home", blogs });
+// });
 
-app.get('/blogs', (req, res) => {
-    Blog.find().sort({ createdAt: -1 })
-        .then((result) => {
-            res.render('index', { title: 'All Blogs', blogs: result })
-        })
-        .catch(err => console.log(err));
-})
 
 app.get('/about', (req, res) => {
     res.render('about', { title: "About" });
 });
 
-app.get('/blogs/create', (req, res) => {
-    res.render('create', { title: "Create a new blog" });
-})
+app.use('/blogs', blogRoutes);
 
 app.use((req, res) => {
-    res.status(404).render('404', { titile: "404" });
+    res.status(404).render('404', { title: "404" });
 })
